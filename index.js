08bcf6b1d8bbb9e9c99545e461a65893d5a85ff5
jfriend00 <jfriend00@users.noreply.github.com>
2020-04-26 16:25:02 -0700

    Add open()
    Change way things are exported so you can do const {fs, fsp, fsc, path} = require('fs-common');

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const path = require('path');
 // get full path listing from a directory
 // options to filter the listing by type
 //    type: "files" | "dirs" | "both"
-async function readDirectory(dir, options = {type: "files"}) {
+async function readDirectory(dir, {type = "files"} = {}) {
+    console.log(type);
     let files = await fsp.readdir(dir, {withFileTypes: true});
-    if (options.type === "files") {
+    if (type === "files") {
         files = files.filter(entry => entry.isFile());
-    } else if (options.type === "dirs") {
+    } else if (type === "dirs") {
         files = files.filter(entry => entry.isDirectory());
     }
     return files.map(entry => {
@@ -44,4 +45,43 @@ async function walk(dir, callback = null, results = []) {
     return results;
 }
 
-module.exports = { readDirectory, walk };
+// call fsp.open() normally, but add a writeBufferCheck method to the handle that
+// automatically checks to see if the proper number of bytes were written to the file
+// We do it this way to avoid modify the fileHandle prototype and affecting other users
+// of that object
+async function open(...args) {
+    let handle = await fsp.open(...args);
+
+    handle.writeBufferCheck = async function(...writeArgs) {
+        let [buffer, offset, length] = writeArgs;
+        if (!Buffer.isBuffer(buffer)) {
+            throw new Error("Can only use writeBufferCheck with a Buffer as first argument");
+        }
+        let result = await this.write(...writeArgs);
+        // length is optional
+        if (!length) {
+            length = buffer.length;
+        }
+        if (result.bytesWritten !== length) {
+            throw new Error(`All bytes not written to file ${writeArgs[0]}./nRequested to write ${length}, actually wrote ${result.bytesWritten}`);
+        }
+        return result;
+    }
+
+    handle.closeLog = function(logFn = console.log.bind(console)) {
+        return this.close().catch(err => {
+            logFn(err);
+            // eat the error, do not propagate it (that is the point of this function)
+        });
+    }
+
+    return handle;
+}
+
+function unlinkIgnore(path) {
+    return fsp.unlink(path).catch(err => {
+        // intentionally eat the error
+    });
+}
+
+module.exports = { fsc: {readDirectory, walk, path, open, unlinkIgnore}, fs, fsp };
